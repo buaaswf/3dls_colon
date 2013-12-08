18971336e6ceac0cdf81e4d1aa281a830f0dfd6b
buaa.swf <876267233@qq.com>
2013-12-08 15:27:59 +0800

    Physical equations
    outer wall detect

---
+++ main.cpp
@@ -0,0 +1,60 @@
+#include "vol_math_LevelSet.h"
+
+#include "test.h"
+#include <iostream>
+#include <crtdbg.h> 
+#include "CImg.h" 
+#include "ThreeDim_LevelSet.h"
+#include "Filter.h"
+#include "WipeNioisePde.h"
+
+using namespace cimg_library;
+using namespace std;
+
+void testcolon(int argc,char **argv)
+{
+	
+	char *pt="single_well";
+	int l=0,m=0,n=0,l1=0,l2=0,iter_outer=10;
+	RawImage test;
+	short * indata=test.readStream("K:\\sdf\\volume\\clean\\WI_3110_S_iso_clean.raw",&l,&m,&n);
+	Raw *initial=new Raw(l,m,n);
+	float *inputo=new float[l*m*n];
+	for (int i = 0; i < l*m*n; i++)
+	{
+		inputo[i]=(float) indata[i];		
+	}
+
+	Raw *input=new Raw(l,m,n,inputo);
+	RawImage *write=new RawImage();
+	ThreeDim_LevelSet *ls=new ThreeDim_LevelSet();
+	ls->initialg(*input);
+	for (int i=0; i<input->getXsize(); i++)
+	{
+		for (int j=0; j<input->getYsize(); j++)
+		{
+			for (int k=0; k<input->getZsize(); k++)
+			{
+				if (input->get(i,j,k)>=1)
+				{
+					initial->put(i,j,k,-2);
+				}
+				else initial->put(i,j,k,2);
+
+			}
+		}
+
+	}
+	*initial=ls->minimal_surface(*initial,*input,5.0,0.1,-3,1.5,1,iter_outer,pt);
+	ls->outerwall(*initial,*input,5.0,0.1,-3,1.5,1,iter_outer,pt);
+	test.writeImage(*initial);
+
+
+}
+
+int main(int argc,char **argv)
+{
+	testcolon(argc,argv);
+	system("pause");
+	return 0;
+}
